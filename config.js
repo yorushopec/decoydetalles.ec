25349f6bb0c6fb0ec2e0e2fa36ee878e3b85eee6
ssaquisi <ssaquisi@espol.edu.ec>
2026-02-03 11:54:55 -0500

    Conectando Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    SUPABASE_URL: '...',
-    SUPABASE_KEY: '...' 
+    SUPABASE_URL: 'https://tolcbwlyuxsmfkkgmbvy.supabase.co',
+    SUPABASE_KEY: 'sb_publishable__th-iNhVyeMcqpJBlXadag_zpzKfgQW' 
 };
